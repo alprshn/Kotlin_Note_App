266c3be1099872c0a45232d8645c182632b1f1ca
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-07-04 07:43:27 +0300

    Edit Toolbar

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,45 +1,31 @@
 package com.example.noteapp
 
-import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.noteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.toolbar.title = ""
-
-        // Custom title
-        val textCustomTitle: TextView = findViewById(R.id.custom_title)
-
-
-
         setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        Glide.with(this)
+            .load(R.drawable.alpersahin)
+            .transform(CircleCrop()) //4
+            .into(binding.imageView)
+
+
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu,menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId;
-        if (id == R.id.toolbar_theme){
-            Toast.makeText(this,"toolbar_theme",Toast.LENGTH_SHORT).show()
-        }
-        if (id == R.id.toolbar_settings){
-            Toast.makeText(this,"toolbar_settings",Toast.LENGTH_SHORT).show()
-        }
-        return true
-    }
+
+
 }
