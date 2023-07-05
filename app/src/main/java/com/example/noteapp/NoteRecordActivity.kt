183c0c58b2af9832dae13be78f1de053d6a4cf88
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-07-05 14:29:22 +0300

    Added Note Record Page

---
+++ app/src/main/java/com/example/noteapp/NoteRecordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.noteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_record)
+    }
+}
