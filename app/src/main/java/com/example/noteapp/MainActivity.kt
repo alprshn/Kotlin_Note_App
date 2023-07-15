53f56835d57ef21c985b8e330600cbbcffcabf8e
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-07-15 13:29:18 +0300

    Edit Entry Page

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -49,21 +49,25 @@ class MainActivity : AppCompatActivity() {
             .transform(CircleCrop())
             .into(binding.imageView)
 
+
+
+
         binding.rv.setHasFixedSize(true)
         binding.rv.layoutManager = LinearLayoutManager(this)
 
 
         vt = HelperDatabase(this)
-        Log.e("deneme", "deneme7")
+        Log.e("deneme","deneme7")
         notesLists = Notesdao().AllNotes(vt)
-        Log.e("deneme", "deneme6")
+        Log.e("deneme","deneme6")
         adapter = NotsAdapter(this, notesLists)
-        Log.e("deneme", "deneme8")
+        Log.e("deneme","deneme8")
         binding.rv.adapter = adapter
-        Log.e("deneme", "deneme9")
+        Log.e("deneme","deneme9")
         binding.floatingActionButton.setOnClickListener {
             startActivity(Intent(this@MainActivity, NoteRecordActivity::class.java))
         }
+
         ChangeViev()
 
 
