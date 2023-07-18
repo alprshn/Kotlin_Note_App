7f5fb556360b68795eba4f2f9d8d5de761d6198f
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-07-18 19:01:49 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/example/noteapp/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.noteapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.noteapp.databinding.ActivitySplashBinding
+
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            teamLiquidIv.alpha = 0f
+            teamLiquidIv.animate().setDuration(1500).alpha(1f).withEndAction{
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                finish()
+            }
+        }
+    }
+}
