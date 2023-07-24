e3ab2c83bbdef273a98845928e206203c1c2cdee
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-07-24 22:08:12 +0300

    Added Dokka and Covarage

--- app/src/androidTest/java/com/example/noteapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/noteapp/ExampleInstrumentedTest.kt
@@ -18,104 +18,105 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     private lateinit var hd: HelperDatabase
     private lateinit var notesDao: Notesdao
-//    @Test
-//    fun testUpdateNote() {
-//        val context = ApplicationProvider.getApplicationContext<Context>()
-//        hd = HelperDatabase(context)
-//        notesDao = Notesdao()
-//        // First, add a note to update
-//        val initialNote = "Initial note"
-//        val initialNoteTitle = "Initial note title"
-//        val initialEmoji = "😊"
-//        val initialNoteDate = "2023-07-23"
-//        val initialNoteColor = 2
-//
-//        notesDao.AddNote(
-//            hd,
-//            initialNote,
-//            initialNoteTitle,
-//            initialEmoji,
-//            initialNoteDate,
-//            initialNoteColor
-//        )
-//
-//        // Get the ID of the added note
-//        val notesList = notesDao.AllNotes(hd)
-//        val noteId = notesList[0].note_id
-//
-//        // Updated values
-//        val updatedNote = "Updated note"
-//        val updatedNoteTitle = "Updated note title"
-//        val updatedEmoji = "😎"
-//        val updatedNoteDate = "2023-07-25"
-//        val updatedNoteColor = 3
-//
-//        notesDao.UpdateNote(
-//            hd,
-//            noteId,
-//            updatedNote,
-//            updatedNoteTitle,
-//            updatedEmoji,
-//            updatedNoteDate,
-//            updatedNoteColor
-//        )
-//
-//        // Check if the note was updated successfully
-//        val updatedNotesList = notesDao.AllNotes(hd)
-//        assertEquals(updatedNote, updatedNotesList[0].note)
-//        assertEquals(updatedNoteTitle, updatedNotesList[0].note_title)
-//        assertEquals(updatedEmoji, updatedNotesList[0].emoji)
-//        assertEquals(updatedNoteDate, updatedNotesList[0].note_date)
-//        assertEquals(updatedNoteColor, updatedNotesList[0].note_color)
-//        Thread.sleep(1000)
-//
-//    }
-
     @Test
-    fun NoteApp_Database_Test_1() {
+    fun testUpdateNote() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         hd = HelperDatabase(context)
         notesDao = Notesdao()
-        val note = "This is a test note."
-        val noteTitle = "Test Note"
-        val emoji = "😃"
-        val noteDate = "2023-07-22"
-        val noteColor = 123456
+        // First, add a note to update
+        val initialNote = "Initial note"
+        val initialNoteTitle = "Initial note title"
+        val initialEmoji = "😊"
+        val initialNoteDate = "2023-07-23"
+        val initialNoteColor = 2
 
-        notesDao.AddNote(hd, note, noteTitle, emoji, noteDate, noteColor)
-
-        val retrievedNotes = notesDao.AllNotes(hd)
-        assertEquals(20, retrievedNotes.size)//+2
-
-
-    }
-
-
-    @Test
-    fun testNoteDelete() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        hd = HelperDatabase(context)
-        notesDao = Notesdao()
-        // Add a note first
-        val note = "Test note"
-        val note_title = "Test note title"
-        val emoji = "😃"
-        val note_date = "2023-07-24"
-        val note_color = 1
-
-        notesDao.AddNote(hd, note, note_title, emoji, note_date, note_color)
+        notesDao.AddNote(
+            hd,
+            initialNote,
+            initialNoteTitle,
+            initialEmoji,
+            initialNoteDate,
+            initialNoteColor
+        )
 
         // Get the ID of the added note
         val notesList = notesDao.AllNotes(hd)
         val noteId = notesList[0].note_id
 
-        // Delete the note
-        notesDao.NoteDelete(hd, noteId)
+        // Updated values
+        val updatedNote = "Updated note"
+        val updatedNoteTitle = "Updated note title"
+        val updatedEmoji = "😎"
+        val updatedNoteDate = "2023-07-25"
+        val updatedNoteColor = 3
 
-        // Check if the note was deleted successfully
-        val deletedNotesList = notesDao.AllNotes(hd)
-        assertEquals(37, deletedNotesList.size)//+4
+        notesDao.UpdateNote(
+            hd,
+            noteId,
+            updatedNote,
+            updatedNoteTitle,
+            updatedEmoji,
+            updatedNoteDate,
+            updatedNoteColor
+        )
+
+        // Check if the note was updated successfully
+        val updatedNotesList = notesDao.AllNotes(hd)
+        assertEquals(updatedNote, updatedNotesList[0].note)
+        assertEquals(updatedNoteTitle, updatedNotesList[0].note_title)
+        assertEquals(updatedEmoji, updatedNotesList[0].emoji)
+        assertEquals(updatedNoteDate, updatedNotesList[0].note_date)
+        assertEquals(updatedNoteColor, updatedNotesList[0].note_color)
+
     }
+
+//    @Test
+//    fun NoteApp_Database_Test_1() {
+//        val context = ApplicationProvider.getApplicationContext<Context>()
+//        hd = HelperDatabase(context)
+//        notesDao = Notesdao()
+//        val note = "This is a test note."
+//        val noteTitle = "Test Note"
+//        val emoji = "😃"
+//        val noteDate = "2023-07-22"
+//        val noteColor = 123456
+//
+//        notesDao.AddNote(hd, note, noteTitle, emoji, noteDate, noteColor)
+//
+//        val retrievedNotes = notesDao.AllNotes(hd)
+//        assertEquals(18, retrievedNotes.size)//+2
+//
+//
+//    }
+//
+//
+//
+//    @Test
+//    fun testNoteDelete() {
+//        val context = ApplicationProvider.getApplicationContext<Context>()
+//        hd = HelperDatabase(context)
+//        notesDao = Notesdao()
+//        // Add a note first
+//        val note = "Test note"
+//        val note_title = "Test note title"
+//        val emoji = "😃"
+//        val note_date = "2023-07-24"
+//        val note_color = 1
+//
+//        notesDao.AddNote(hd, note, note_title, emoji, note_date, note_color)
+//
+//        // Get the ID of the added note
+//        val notesList = notesDao.AllNotes(hd)
+//        val noteId = notesList[0].note_id
+//
+//        // Delete the note
+//        notesDao.NoteDelete(hd, noteId)
+//
+//        // Check if the note was deleted successfully
+//        val deletedNotesList = notesDao.AllNotes(hd)
+//        assertEquals(33, deletedNotesList.size)//+4
+//    }
+//
 
 
 }
